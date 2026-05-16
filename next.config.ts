6645e1417142bcc4d9fad194996b8d4a1f087424
dwikiputraa18 <dwikiputra678@gmail.com>
2026-05-16 22:19:05 +0700

    next commit

--- next.config.ts
+++ next.config.ts
@@ -3,21 +3,19 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  // PWA dinonaktifkan HANYA saat development, aktif saat production build
+  // PWA dinonaktifkan saat development, aktif saat production build
   disable: process.env.NODE_ENV === "development",
+  // Opsi registrasi otomatis ditaruh di sini (jika versi lama memicu error, baris bawah ini bisa dihapus aman karena default-nya sudah true)
   register: true,
 });
 
 const nextConfig: NextConfig = {
-  // 1. Solusi Utama: Menambahkan konfigurasi turbopack kosong untuk membungkam error Webpack
-  turbopack: {},
-
+  // Properti turbopack dihapus karena kita menggunakan compiler Webpack murni via terminal flag
   experimental: {
     serverActions: {
-      // 2. Koreksi Hostname: Next.js membutuhkan raw hostname/domain tanpa subnet mask CIDR
       allowedOrigins: [
         "localhost:3000",
-        "192.168.18.75:3000", // Sesuaikan dengan IP Network laptop Anda yang terdeteksi kemarin
+        "192.168.18.75:3000",
         "127.0.0.1:3000"
       ]
     }
